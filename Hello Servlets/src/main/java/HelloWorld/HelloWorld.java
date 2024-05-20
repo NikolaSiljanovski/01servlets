2a8128b75afc54f59046be5fc74be039c468bc39
Nikola Siljanovski <146115350+NikolaSiljanovski@users.noreply.github.com>
2024-05-20 09:44:44 +0200

    Add files via upload

---
+++ Hello Servlets/src/main/java/HelloWorld/HelloWorld.java
@@ -0,0 +1,34 @@
+package HelloWorld;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HelloWorld() {
+
+    }
+
+	/**
+	 * @see HttpServle	t#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		System.out.println("Hello World");
+		System.out.println("Your server is on.");
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
